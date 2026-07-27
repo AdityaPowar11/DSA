46c759fe4319ed98af8da49189e94682299f0992
AdityaPowar11 <adityapowar501@gmail.com>
2026-07-27 20:51:15 +0530

    Time: 1 ms (66.20%) | Memory: 44.6 MB (60.68%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.java
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.java
@@ -1,51 +1,24 @@
 class Solution {
-    public static void merge(int[] arr, int left, int mid, int right) {
-    int[] temp = new int[right - left + 1];
-
-    int i = left;
-    int j = mid + 1;
-    int k = 0;
-
-    while (i <= mid && j <= right) {
-        if (arr[i] <= arr[j]) {
-            temp[k++] = arr[i++];
-        } else {
-            temp[k++] = arr[j++];
-        }
-    }
-
-    while (i <= mid) {
-        temp[k++] = arr[i++];
-    }
-
-    while (j <= right) {
-        temp[k++] = arr[j++];
-    }
-
-    // Copy back to original array
-    for (int x = 0; x < temp.length; x++) {
-        arr[left + x] = temp[x];
-    }
-}
-public static void mergeSort(int[] arr, int left, int right) {
-    if (left >= right) {
-        return;
-    }
-
-    int mid = left + (right - left) / 2;
-
-    mergeSort(arr, left, mid);       // Sort left half
-    mergeSort(arr, mid + 1, right);  // Sort right half
-
-    merge(arr, left, mid, right);    // Merge the two sorted halves
-}
+   
     public int maxProduct(int[] arr) {
 
-        mergeSort(arr,0,arr.length-1);
+        int max1= Integer.MIN_VALUE;
+        int max2 =Integer.MIN_VALUE;
 
-        int ans = (arr[arr.length-1]-1) * (arr[arr.length-2]-1);
 
-        return ans;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max2){
+                max2= arr[i];
+            }
+            if(max2>max1){
+                int temp = max1;
+                max1=max2;
+                max2=temp;
+            }
+        }
+
+
+        return (max1-1)*(max2-1);
 
     
         
