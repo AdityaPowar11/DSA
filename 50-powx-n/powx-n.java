d1b07b0999b5e3990d2c37745a5d742a402fe7e4
AdityaPowar11 <adityapowar501@gmail.com>
2026-07-27 12:52:31 +0530

    Time: 0 ms (100.00%) | Memory: 47.9 MB (65.21%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,22 @@
+class Solution {
+    public double myPow(double x, int n) {
+   long power = n; // avoid overflow
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+
+        double res = 1;
+
+        while (power > 0) {
+           if ((power & 1) == 1) { // if odd
+                res *= x;
+            }
+            x *= x;
+            power >>= 1; // divide by 2
+        }
+
+        return res;
+    }
+        
+    }
