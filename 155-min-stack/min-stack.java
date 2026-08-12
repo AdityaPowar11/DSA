61edc988e763e0a54f3e61c30cd230aafe25ccb6
AdityaPowar11 <adityapowar501@gmail.com>
2026-08-13 00:36:32 +0530

    Time: 33 ms (71.29%) | Memory: 102.5 MB (20.52%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,43 +1,55 @@
 class MinStack {
 
-    Stack<Integer> st = new Stack <>();
-    Stack<Integer> min = new Stack <>();
-
+    Stack<Long> st = new Stack<>();
+    long min = Long.MAX_VALUE;
 
     public MinStack() {
-
-        
-        
     }
-    
+
     public void push(int value) {
-        if(st.isEmpty()){
-            st.push(value);
-            min.push(value);
+        long x = (long)value;
+        if (st.isEmpty()) {
+            st.push(x);
+            min =x;
+            return;
+        } else if (x >= min) {
+            st.push(x);
+        } else {
+            st.push(2 * x - min);
+            min = x;
+
         }
-        else{
-            st.push(value);
-            if(min.peek()<value)min.push(min.peek());
-            else min.push(value);
-        }
-        
+
     }
-    
+
     public void pop() {
-        st.pop();
-        min.pop();
-        
+
+        if (st.isEmpty())
+            return;
+        if (st.peek() >= min) {
+            st.pop();
+        } else {
+            min = 2 * min - st.peek();
+            st.pop();
+        }
+
     }
-    
+
     public int top() {
-        return st.peek();
+
+        if (st.isEmpty())
+            return -1;
+
+        long q = st.peek();
+        if (q >= min)
+            return (int) q;
+        else {
+            return (int)min;
+        }
     }
-    
+
     public int getMin() {
-
-        return min.peek();
-
-        
+        return (int) min;
     }
 }
 
