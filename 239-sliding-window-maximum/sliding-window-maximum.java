5c01a436979b22aa2dd291a26350074fd8707a43
AdityaPowar11 <adityapowar501@gmail.com>
2026-08-18 01:45:34 +0530

    Time: 518 ms (5.01%) | Memory: 149.4 MB (41.30%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,45 +1,37 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
 
-        int n = nums.length;
-        int[] ans = new int[n - k + 1];
+        int n =nums.length;
+        int ans[]=new int [n-k+1];
+        int z=0;
 
-        // NGE array
-        int[] nge = new int[n];
         Stack<Integer> st = new Stack<>();
+        int[] nge= new int[n];
+        st.push(n-1);
+        nge[n-1]=n;
 
-        st.push(n - 1);
-        nge[n - 1] = n;
+        for(int i=n-2;i>=0;i--){
 
-        for (int i = n - 2; i >= 0; i--) {
+            while(st.size()!=0 && nums[i]>nums[st.peek()])  st.pop();
 
-            while (!st.isEmpty() && nums[i] >= nums[st.peek()]) {
-                st.pop();
-            }
-
-            if (st.isEmpty())
-                nge[i] = n;
-            else
-                nge[i] = st.peek();
-
+            if(st.size()==0) nge[i] =n;
+            else nge[i]=st.peek();
             st.push(i);
         }
 
-        // Find maximum for every window
-        int j = 0;
 
-        for (int i = 0; i < ans.length; i++) {
+        int j=0;
 
-            if (j < i)
-                j = i;
-
-            while (nge[j] < i + k) {
+        for(int i=0;i<n-k+1;i++){
+            if(j>=i+k) j =i;
+            int max =nums[j];
+            while(j<i+k){
+                max=nums[j];
                 j = nge[j];
             }
-
-            ans[i] = nums[j];
+            ans[z++] =max;
         }
-
         return ans;
+
     }
 }
